fe9e3ee0b2c5ad5f66e421257e0935f3f805818f
Syed-Hamza10 <syed.hamza.ds@gmail.com>
2024-12-28 16:30:16 +0500

    Completed Proxy Server

---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+
+    printf("Tryna print something since its been decades, not literally, I used C. It was last in Punjab College by Sir Bilal");
+    return 0;
+
+}
